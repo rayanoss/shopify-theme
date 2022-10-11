ac305945f0ad7356b22e0df212736e94772602aa
rayanoss <zeroko713@gmail.com>
2022-10-11 14:04:48 +0200

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,14 @@ module.exports = {
     enabled: true,
     content: ["./**/*.liquid"],
   },
+  theme: {
+    extend: {
+      backgroundImage: {
+        'strips-accordion': "url('https://cdn.shopify.com/s/files/1/0916/4078/files/Strips_Accordion_-_M.jpg?v=1634701615')",
+      }, 
+      backgroundPosition: {
+        'bg-center-slider': "center 75%",
+      }, 
+    }
+  }
 };
